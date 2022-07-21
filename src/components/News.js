4ea2817d78079ee14f5a6e078f508e3cd3244dba
Rohit Tewari <rtewari056@gmail.com>
2022-07-21 23:07:28 +0530

    Adding Previous & Next Buttons to populate NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -2,12 +2,26 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 
 export class News extends Component {
+  apiKey = "95a1932962814593a37f42c357e22595";
+  country = "in";
   constructor() {
     super();
     this.state = {
       articles: [], // At first articles will be an empty array
       loading: false, // And loading will be false
+      pageNumber: 1, // And pageNumber will be 1
     };
+  }
+
+  async componentDidMount() {
+    const apiURL = `https://newsapi.org/v2/top-headlines?country=${this.country}&apiKey=${this.apiKey}&page=1&pageSize=20`;
+    const data = await fetch(apiURL);
+    const parsedData = await data.json();
+
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
 
   // Function to convert iso 8601 date format
@@ -18,13 +32,42 @@ export class News extends Component {
     return `${d.toUTCString()}`;
   };
 
-  async componentDidMount() {
-    const apiURL = "https://newsapi.org/v2/top-headlines?country=in&apiKey=95a1932962814593a37f42c357e22595";
+  handlePreviousClick = async () => {
+    const apiURL = `https://newsapi.org/v2/top-headlines?country=${
+      this.country
+    }&apiKey=${this.apiKey}&page=${this.state.pageNumber - 1}&pageSize=20`;
     const data = await fetch(apiURL);
     const parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
-    console.log(parsedData);
-  }
+
+    this.setState({
+      articles: parsedData.articles,
+      pageNumber: this.state.pageNumber - 1,
+    });
+  };
+
+  handleNextClick = async () => {
+    // 
+    if (this.state.pageNumber + 1 > Math.ceil(this.state.totalResults / 20)) {
+      // If there is no articles left to populate on the page then this chunk of code will run
+      /*
+      Suppose totalResults = 21 So, Math.ceil(21/20) will be equals 2
+        At first, page = 1 is not greater than 2
+        After clicking on the "Next" button, page = 2 which is also not greater than 2
+        Again clicking on the "Next" button, page = 3 which is greater than 2, so now there is no articles left to populate on the screen
+      */
+    } else {
+      const apiURL = `https://newsapi.org/v2/top-headlines?country=${
+        this.country
+      }&apiKey=${this.apiKey}&page=${this.state.pageNumber + 1}&pageSize=20`;
+      const data = await fetch(apiURL);
+      const parsedData = await data.json();
+
+      this.setState({
+        articles: parsedData.articles,
+        pageNumber: this.state.pageNumber + 1,
+      });
+    }
+  };
 
   render() {
     return (
@@ -48,6 +91,23 @@ export class News extends Component {
               />
             );
           })}
+          <div className="container mt-5 d-flex justify-content-between">
+            <button
+              type="button"
+              className="btn btn-dark"
+              onClick={this.handlePreviousClick}
+              disabled={this.state.pageNumber <= 1}
+            >
+              &laquo; Previous
+            </button>
+            <button
+              type="button"
+              className="btn btn-dark"
+              onClick={this.handleNextClick}
+            >
+              Next &raquo;
+            </button>
+          </div>
         </div>
       </div>
     );
